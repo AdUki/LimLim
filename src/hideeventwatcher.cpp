a14bd14fe390aeeadd24bf1e294b43bf926354b9
Šimon Mikuda <simon.mikuda@gmail.com>
2012-04-15 13:51:30 +0200

    Added class for watching hide event

---
+++ src/hideeventwatcher.cpp
@@ -0,0 +1,23 @@
+#include "hideeventwatcher.h"
+#include <QEvent>
+
+HideEventWatcher::HideEventWatcher(QObject *parent)
+    : QObject(parent)
+{
+}
+
+bool HideEventWatcher::eventFilter(QObject *obj, QEvent *event)
+{
+    Q_UNUSED(obj);
+    if (event->type() == QEvent::Hide) {
+        emit isHidden(true);
+        emit isShown(false);
+        emit hidden();
+        return true;
+    } else if (event->type() == QEvent::Show) {
+        emit isHidden(false);
+        emit isShown(true);
+        emit shown();
+        return true;
+    }
+}
