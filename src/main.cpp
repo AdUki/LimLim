db25832e2baae0e31b1e7de342af3be73354d859
Šimon Mikuda <simon.mikuda@gmail.com>
2012-06-13 11:42:22 +0200

    Removed global.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,12 @@
 #include <QDir>
 #include <QDebug>
 #include "luacontrol.h"
-#include "global.h"
 
 QString APP_DIR_PATH;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    APP_DIR_PATH = QApplication::applicationDirPath();
-
-#ifdef Q_WS_WIN
-    APP_DIR_PATH.replace(QRegExp("[^/.]+$"),"");
-    APP_DIR_PATH.replace('/','\\');
-#else
-    APP_DIR_PATH.append(QDir::separator());
-#endif
 
     LuaControl *mainWin = new LuaControl;
 
